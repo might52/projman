c71c71121f206664c955e40709362af69d7fae7b
nik709 <nikitagrechukhin@rambler.ru>
2019-05-28 00:06:04 +0300

    add view models

--- services/src/main/java/org/might/projman/controllers/ProjectRestController.java
+++ services/src/main/java/org/might/projman/controllers/ProjectRestController.java
@@ -3,14 +3,19 @@ package org.might.projman.controllers;
 
 import org.might.projman.controllers.annotations.Auth;
 import org.might.projman.controllers.dtos.ProjectDTOResponse;
+import org.might.projman.dba.model.Status;
+import org.might.projman.dba.model.Task;
 import org.might.projman.services.ProjectRoleService;
 import org.might.projman.services.ProjectService;
+import org.might.projman.services.StatusService;
 import org.might.projman.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -21,13 +26,16 @@ public class ProjectRestController {
     private ProjectService projectService;
     private TaskService taskService;
     private ProjectRoleService projectRoleService;
+    private StatusService statusService;
 
     public ProjectRestController(TaskService taskService,
                                  ProjectRoleService projectRoleService,
-                                 ProjectService projectService) {
+                                 ProjectService projectService,
+                                 StatusService statusService) {
         this.projectService = projectService;
         this.taskService = taskService;
         this.projectRoleService = projectRoleService;
+        this.statusService = statusService;
     }
 
     @RequestMapping(value = "/projects")
@@ -38,6 +46,32 @@ public class ProjectRestController {
                         ProjectDTOResponse.createFromEntity(project, taskService.getAll(), projectRoleService.getAll()) : null)
                 .filter(Objects::nonNull)
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/update_task_time")
+    @ResponseBody
+    public String logTime(@RequestParam(value = "task_id") long taskId, @RequestParam(value = "time") int time) {
+        Task task = taskService.getTaskById(taskId);
+        task.setUsedTime(new Date(task.getCreationDate().getTime() + time*60*1000));
+        taskService.saveTask(task);
+        return "ok";
+    }
+
+    @GetMapping(value = "/update_task_status")
+    @ResponseBody
+    public String updateStatus(@RequestParam(value = "task_id") long taskId, @RequestParam(value = "status_name") String statusName) {
+        Task task = taskService.getTaskById(taskId);
+        Optional<Status> optionalStatus = statusService.getAll()
+                .stream()
+                .filter(status -> status.getName().startsWith(statusName))
+                .findFirst();
+        if (optionalStatus.isPresent()) {
+            task.setStatusId(optionalStatus.get());
+            taskService.saveTask(task);
+            return "ok";
+        } else {
+            return "Can not find status by name";
+        }
     }
 
 }
